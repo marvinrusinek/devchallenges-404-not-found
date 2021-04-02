f4b4e72a8480763c1b6871e0e2493fb9543cba7a
Marvin Rusinek <marvinrusinek@gmail.com>
2021-04-02 13:55:28 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './404-not-found/containers/container/container.component';
+import { HeaderComponent } from './404-not-found/containers/header/header.component';
+import { MainComponent } from './404-not-found/containers/main/main.component';
+import { FooterComponent } from './404-not-found/containers/footer/footer.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '404-not-found',
+    pathMatch: 'full'
+  },
+  {
+    path: '404-not-found',
+    loadChildren: () =>
+      import('./404-not-found/routing/404-not-found-routing.module').then(m => m.DevChallenges404NotFoundRoutingModule)
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
